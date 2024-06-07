5a6d4824b8746e8b0d6e73c544c6ec964f345c9d
hbatistuzzo <hbatistuzzo@gmail.com>
2024-06-07 16:56:57 -0300

    alterações no test_products de usecases para generalização

--- tests/schemas/test_product.py
+++ tests/schemas/test_product.py
@@ -1,5 +1,4 @@
 from pydantic import ValidationError
-
 import pytest
 from store.schemas.product import ProductIn
 from tests.factories import product_data
@@ -7,14 +6,15 @@ from tests.factories import product_data
 
 def test_schemas_return_success():
     data = product_data()
-    product = ProductIn.model_validate(data)
+    product = ProductIn.model_validate(
+        data
+    )  # equivalent to: product = ProductIn.(**data)
 
-    assert product.name == "Iphone 14 Pro Max"
+    assert product.name == data["name"]
 
 
 def test_schemas_return_raise():
-    data = {"name": "Iphone 14 Pro Max", "quantity": 10, "price": 8.500}
-
+    data = {"name": "iPhone 14 Pro Max", "quantity": 10, "price": "8.499"}
     with pytest.raises(ValidationError) as err:
         ProductIn.model_validate(data)
 
@@ -22,6 +22,6 @@ def test_schemas_return_raise():
         "type": "missing",
         "loc": ("status",),
         "msg": "Field required",
-        "input": {"name": "Iphone 14 Pro Max", "quantity": 10, "price": 8.5},
-        "url": "https://errors.pydantic.dev/2.5/v/missing",
+        "input": {"name": "iPhone 14 Pro Max", "quantity": 10, "price": "8.499"},
+        "url": "https://errors.pydantic.dev/2.7/v/missing",
     }
--- tests/usecases/test_product.py
+++ tests/usecases/test_product.py
@@ -1,34 +1,34 @@
 from typing import List
-from uuid import UUID
+import uuid
 
 import pytest
-from store.core.exceptions import NotFoundException
 from store.schemas.product import ProductOut, ProductUpdateOut
 from store.usecases.product import product_usecase
+
+from tests.factories import product_data
+from store.core.exceptions import NotFoundException
 
 
 async def test_usecases_create_should_return_success(product_in):
     result = await product_usecase.create(body=product_in)
 
     assert isinstance(result, ProductOut)
-    assert result.name == "Iphone 14 Pro Max"
+    assert result.name == product_data()["name"]
 
 
 async def test_usecases_get_should_return_success(product_inserted):
     result = await product_usecase.get(id=product_inserted.id)
 
     assert isinstance(result, ProductOut)
-    assert result.name == "Iphone 14 Pro Max"
+    assert result.name == product_data()["name"]
 
 
-async def test_usecases_get_should_not_found():
+async def test_usecases_get_should_return_not_found():
+    random_uid = uuid.uuid4()
     with pytest.raises(NotFoundException) as err:
-        await product_usecase.get(id=UUID("1e4f214e-85f7-461a-89d0-a751a32e3bb9"))
+        await product_usecase.get(id=random_uid)
 
-    assert (
-        err.value.message
-        == "Product not found with filter: 1e4f214e-85f7-461a-89d0-a751a32e3bb9"
-    )
+    assert err.value.message == f"Product not found with filter UUID({random_uid})"
 
 
 @pytest.mark.usefixtures("products_inserted")
@@ -40,7 +40,7 @@ async def test_usecases_query_should_return_success():
 
 
 async def test_usecases_update_should_return_success(product_up, product_inserted):
-    product_up.price = "7.500"
+    product_up.price = 7.500
     result = await product_usecase.update(id=product_inserted.id, body=product_up)
 
     assert isinstance(result, ProductUpdateOut)
@@ -48,15 +48,12 @@ async def test_usecases_update_should_return_success(product_up, product_inserte
 
 async def test_usecases_delete_should_return_success(product_inserted):
     result = await product_usecase.delete(id=product_inserted.id)
-
     assert result is True
 
 
 async def test_usecases_delete_should_not_found():
+    random_uid = uuid.uuid4()
     with pytest.raises(NotFoundException) as err:
-        await product_usecase.delete(id=UUID("1e4f214e-85f7-461a-89d0-a751a32e3bb9"))
+        await product_usecase.delete(id=random_uid)
 
-    assert (
-        err.value.message
-        == "Product not found with filter: 1e4f214e-85f7-461a-89d0-a751a32e3bb9"
-    )
+    assert err.value.message == f"Product not found with filter UUID({random_uid})"
